50ce3786d6d706ed3e1d17f60ddedbdecbd78523
Qudus Lawal <lawalqudus980@gmail.com>
2023-05-12 04:41:28 +0100

    Update code block

--- scripts/signInUp.js
+++ scripts/signInUp.js
@@ -1,3 +1,5 @@
 function myfunction() {
-    var x = document.getElementById('mypassword')
+    var x = document.getElementById('mypassword');
+    var y = document.getElementById('hide1');
+    var z = document.getElementById('hide2');
 }
