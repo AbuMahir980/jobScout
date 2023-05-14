bd1aabad700224b4046c56e6b308335d7ced28f0
Qudus Lawal <lawalqudus980@gmail.com>
2023-05-14 15:35:33 +0100

    Debugging code

--- scripts/signInUp.js
+++ scripts/signInUp.js
@@ -14,18 +14,3 @@ function myfunction() {
     }
 }
 
-function myfunction1(){
-    var a = document.getElementById('mypassword1');
-    var b = document.getElementById('hide3');
-    var c = document.getElementById('hide4');
-    if(a.type === 'passwordSec'){
-        a.type = "text";
-        b.style.display = "block";
-        c.style.display = "none";
-    }
-    else{
-        a.type = "passwordSec";
-        b.style.display = "none";
-        c.style.display = "block";
-    }
-}
