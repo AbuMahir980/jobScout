4f4dba76549e4f7989a71463dbd82a27d7328c65
Qudus Lawal <lawalqudus980@gmail.com>
2023-05-14 15:44:24 +0100

    Code debugged

--- scripts/signUp.js
+++ scripts/signUp.js
@@ -1,14 +1,30 @@
+function myfunction() {
+    var x = document.getElementById('mypassword');
+    var y = document.getElementById('hide1');
+    var z = document.getElementById('hide2');
+    if(x.type === 'password'){
+        x.type = "text";
+        y.style.display = "block";
+        z.style.display = "none";
+    }
+    else{
+        x.type = "password";
+        y.style.display = "none";
+        z.style.display = "block";
+    }
+}
+
 function myfunction1(){
-    var a = document.getElementById('mypassword1');
+    var a = document.getElementById('my_password');
     var b = document.getElementById('hide3');
     var c = document.getElementById('hide4');
-    if(a.type === 'passwords'){
+    if(a.type === 'password'){
         a.type = "text";
         b.style.display = "block";
         c.style.display = "none";
     }
     else{
-        a.type = "passwords";
+        a.type = "password";
         b.style.display = "none";
         c.style.display = "block";
     }
