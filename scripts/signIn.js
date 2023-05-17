8adb412a5f17242c89543cc7b1c71e786a98bada
Qudus Lawal <lawalqudus980@gmail.com>
2023-05-17 12:27:54 +0100

    Edit codeblocks

--- scripts/signIn.js
+++ scripts/signIn.js
@@ -1,15 +1,15 @@
-function myfunction() {
-    var x = document.getElementById('mypassword');
-    var y = document.getElementById('hide1');
-    var z = document.getElementById('hide2');
+function togglePasswordVisibility() {
+    var newPassword = document.getElementById('mypassword');
+    var passwordHidden = document.getElementById('hide1');
+    var passwordVisible = document.getElementById('hide2');
     if(x.type === 'password'){
-        x.type = "text";
-        y.style.display = "block";
-        z.style.display = "none";
+        newPassword.type = "text";
+        passwordHidden.style.display = "block";
+        passwordVisible.style.display = "none";
     }
     else{
-        x.type = "password";
-        y.style.display = "none";
-        z.style.display = "block";
+        newPassword.type = "password";
+        passwordHidden.style.display = "none";
+        passwordVisible.style.display = "block";
     }
 }
